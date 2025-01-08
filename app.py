429a3ad6162096930369af6b80294b888cb23649
Maxim Gordeev <m.tehnologiy@gmail.com>
2025-01-08 21:55:34 +0300

    app.py

--- app.py
+++ app.py
@@ -1,7 +1,9 @@
 from flask import Flask, request, jsonify
 import requests
+from flask_cors import CORS
 
 app = Flask(__name__)
+CORS(app)
 
 # Конфигурация API
 YANDEX_API_URL = "https://llm.api.cloud.yandex.net/foundationModels/v1/completion"  # Замените на актуальный URL API
